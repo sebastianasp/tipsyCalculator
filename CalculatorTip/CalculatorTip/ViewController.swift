6fd186d310b19d319656cde9899acd65345fb83a
sebastianasp <sebastian@sjundegatan.se>
2017-03-19 08:30:31 +0100

    first commit

---
+++ CalculatorTip/CalculatorTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorTip
+//
+//  Created by Sebastian Asp on 2017-03-19.
+//  Copyright © 2017 Sebastian Asp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
